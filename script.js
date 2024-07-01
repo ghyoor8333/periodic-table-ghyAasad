f4254578bf76594b34879ccf000f8cedc4938bdb
Ghyoor Qasim <115895098+ghyoor8333@users.noreply.github.com>
2024-07-02 01:59:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.elements').click(function () {
+        $('.details-div').html("HI I AM NTH ELEMENT OF PERIODIC TABLE AND I AM A GAS NON METAL");
+    });
+});
